20085070b482759fd4a8e15d91bfc86d204bf1b5
katthy <you@example.com>
2020-08-26 20:17:59 -0400

    tercer dia

--- app/src/main/java/com/example/checkapartment/FirstFragment.java
+++ app/src/main/java/com/example/checkapartment/FirstFragment.java
@@ -7,17 +7,33 @@ import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 import androidx.navigation.fragment.NavHostFragment;
 
-public class FirstFragment extends Fragment {
+import com.example.checkapartment.databinding.FragmentFirstBinding;
 
+
+public class FirstFragment extends Fragment {
+  private FragmentFirstBinding mbinding2;
     @Override
+
+   //View onCreateView
     public View onCreateView(
             LayoutInflater inflater, ViewGroup container,
             Bundle savedInstanceState
     ) {
+        //proyecto todos los componentes en la vista
+
+        mbinding2=FragmentFirstBinding.inflate(inflater,container,false);
+        mbinding2.Recyclerid.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Navigation.findNavController(mbinding2.getRoot()).navigate(R.id.action_firstFragment_to_secondFragment2);
+            }
+        });
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_first, container, false);
+        return mbinding2.getRoot();
     }
 
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
