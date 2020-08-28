f8edbb2468d3d392942f489ccb8d0a0036d3d636
katthy <you@example.com>
2020-08-28 16:39:44 -0400

    se sigue avanzando.

--- app/src/main/java/com/example/checkapartment/SecondFragment.java
+++ app/src/main/java/com/example/checkapartment/SecondFragment.java
@@ -9,21 +9,35 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
 
-public class SecondFragment extends Fragment {
+import com.bumptech.glide.Glide;
+import com.example.checkapartment.databinding.FragmentSecondBinding;
 
+
+public class SecondFragment extends Fragment {
+    private FragmentSecondBinding mbinding3;
+    private String url;
     @Override
-    public View onCreateView(
-            LayoutInflater inflater, ViewGroup container,
-            Bundle savedInstanceState
-    ) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,Bundle savedInstanceState) {
+          //traer la foto
+        if(getArguments()!= null){
+            url=getArguments().getString("URL");
+        }
+
+
+        //proyecto todos los componentes en la vista
+
+        mbinding3= FragmentSecondBinding.inflate(inflater,container,false);
+       // Glide.with(getContext()).load(url).into(mbinding3.imagen2);
+        //Glide.with(holder.itemView.getContext()).load(aparment.getUrlImageBuilding()).into(holder.imagen2);
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_second, container, false);
+        return mbinding3.getRoot();
     }
 
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        view.findViewById(R.id.button2).setOnClickListener(new View.OnClickListener() {
+        view.findViewById(R.id.alerta).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 NavHostFragment.findNavController(SecondFragment.this)
